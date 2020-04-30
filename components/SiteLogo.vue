6abe15e30752a957adc7f8941a67f7bf40eeb4cf
bsilvade <bds@outlook.com>
2020-04-30 15:04:38 +0100

    Update SiteLogo.vue

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -7,15 +7,7 @@
       }"
     >
       <img src="/logo-frames/greenapplogo.jpeg"
-        //v-for="x in frames"
-        //:key="x"
-        //:src="`/logo-frames/logo-f${x - 1}.svg`"
       />
-      //<img
-        //v-for="x in frames"
-        //:key="`${x}-r`"
-       // :src="`/logo-frames/logo-f${frames - x}.svg`"
-      ///>
     </div>
   </div>
 </template>
