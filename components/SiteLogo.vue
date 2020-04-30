36b82ad36424d9fd65cbbc31c97093caad28fd2f
bsilvade <bds@outlook.com>
2020-04-30 15:15:34 +0100

    Update SiteLogo.vue

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -3,38 +3,13 @@
     <div
       :class="{
         'logo-inner': true,
-        blink: blinking
       }"
     >
-      <img src="/logo-frames/greenapplogo.jpeg"
-      />
+      <img src="/logo-frames/greenapplogo.jpeg"/>
     </div>
   </div>
 </template>
-<script>
-export default {
-  name: 'SiteLogo',
-  data() {
-    return {
-      frames: 5,
-      currentFrame: 1,
-      blinking: false
-    }
-  },
-  mounted() {
-    setInterval(() => {
-      const rand = Math.ceil(Math.random() * 10)
-      this.blinking = rand % 2
-    }, 1000)
-  }
-}
-</script>
 <style lang="scss">
-@keyframes logoBlink {
-  100% {
-    transform: translateY(-360px);
-  }
-}
 .logo-wrapper {
   position: relative;
   height: 50px;
@@ -48,9 +23,6 @@ export default {
       height: 40px;
       margin: 5px;
       max-height: initial;
-    }
-    &.blink {
-      animation: logoBlink 0.2s steps(8, end) alternate;
     }
   }
 }
