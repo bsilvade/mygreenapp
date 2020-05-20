139921367cce680533772e4f046e2f5c245f2f63
bsilvade <bds@outlook.com>
2020-05-20 20:25:20 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
       <template v-slot:default>
         <center>
         <h1 class="title animated fadeInUp">
-          Video coming soon!
+          More coming soon!
           Stay tuned
         </h1>
         <p class="line-1 anim-typewriter">Stay tuned</p>
@@ -57,5 +57,32 @@ export default {
 .home-page .under-subtitle {
   border-top: none;
 }
+.line-1{
+    position: relative;
+    top: 50%;  
+    width: 24em;
+    margin: 0 auto;
+    border-right: 2px solid rgba(255,255,255,.75);
+    font-size: 180%;
+    text-align: center;
+    white-space: nowrap;
+    overflow: hidden;
+    transform: translateY(-50%);    
+}
+
+/* Animation */
+.anim-typewriter{
+  animation: typewriter 4s steps(44) 1s 1 normal both,
+             blinkTextCursor 500ms steps(44) infinite normal;
+}
+@keyframes typewriter{
+  from{width: 0;}
+  to{width: 24em;}
+}
+@keyframes blinkTextCursor{
+  from{border-right-color: rgba(255,255,255,.75);}
+  to{border-right-color: transparent;}
+}
+
 
 </style>
