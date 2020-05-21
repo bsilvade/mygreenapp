b1707ccce54ab5e66ef5709173f097088ccf3c92
bsilvade <bds@outlook.com>
2020-05-21 18:08:26 +0100

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,52 +1,16 @@
 <template>
-<form name="contactUs" method="POST" data-netlify="true" netlify-honeypot="bot-field">
-  <p class="hidden">
-    <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
-  </p>    <!-- Name -->
-    <div class="field">
-      <label class="label is-medium">Name</label>
-      <div class="control has-icons-left">
-        <input class="input is-medium" type="text" placeholder="" />
-        <span class="icon is-small is-left">
-          <font-awesome-icon icon="user" />
-        </span>
-      </div>
-    </div>
-    <!-- Email -->
-    <div class="field">
-      <label class="label is-medium">Email</label>
-      <div class="control has-icons-left">
-        <input
-          class="input is-medium"
-          type="email"
-          placeholder=""
-        />
-        <span class="icon is-small is-left">
-          <font-awesome-icon icon="envelope" />
-        </span>
-      </div>
-    </div>
-    <!-- Message -->
-    <div class="field">
-      <label class="label is-medium">Message</label>
-      <div class="control">
-        <textarea
-          name= "message"
-          class="textarea"
-          placeholder="Detailed description of your comment, request, etc"
-          rows="8"
-        ></textarea>
-      </div>
-    </div>
-    <div class="control">
-      <button class="button is-primary">
-        Contact Us
-      </button>
-    </div>
-  </form>
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" class="input is-medium" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" class="input is-medium"/></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message" placeholder="Detailed description of your comment, request, etc" rows="8"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 </template>
-<style>
- .hidden{
- visibility:hidden;
- }
-</style>
