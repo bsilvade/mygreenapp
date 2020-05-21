88fd8bdb7e8d0722fe4ef95bf51f85651e9bb4ae
bsilvade <bds@outlook.com>
2020-05-21 09:58:18 +0100

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,22 +1,44 @@
 <template>
-  <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
+<form name="contactUs" method="POST" data-netlify="true">
+    <!-- Name -->
+    <div class="field">
+      <label class="label is-medium">Name</label>
+      <div class="control has-icons-left">
+        <input class="input is-medium" type="text" placeholder="" />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="user" />
+        </span>
+      </div>
+    </div>
+    <!-- Email -->
+    <div class="field">
+      <label class="label is-medium">Email</label>
+      <div class="control has-icons-left">
+        <input
+          class="input is-medium"
+          type="email"
+          placeholder=""
+        />
+        <span class="icon is-small is-left">
+          <font-awesome-icon icon="envelope" />
+        </span>
+      </div>
+    </div>
+    <!-- Message -->
+    <div class="field">
+      <label class="label is-medium">Message</label>
+      <div class="control">
+        <textarea
+          class="textarea"
+          placeholder="Detailed description of your comment, request, etc"
+          rows="8"
+        ></textarea>
+      </div>
+    </div>
+    <div class="control">
+      <button class="button is-primary" type="submit">
+        Contact Us
+      </button>
+    </div>
+  </form>
 </template>
