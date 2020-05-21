788ef115184f5432a72998a8f515e6ccf68c78af
bsilvade <bds@outlook.com>
2020-05-21 09:54:42 +0100

    Update ContactForm.vue

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,46 +1,4 @@
 <template>
-  <form>
-    <!-- Name -->
-    <div class="field">
-      <label class="label is-medium">Name</label>
-      <div class="control has-icons-left">
-        <input class="input is-medium" type="text" placeholder="" />
-        <span class="icon is-small is-left">
-          <font-awesome-icon icon="user" />
-        </span>
-      </div>
-    </div>
-    <!-- Email -->
-    <div class="field">
-      <label class="label is-medium">Email</label>
-      <div class="control has-icons-left">
-        <input
-          class="input is-medium"
-          type="email"
-          placeholder=""
-        />
-        <span class="icon is-small is-left">
-          <font-awesome-icon icon="envelope" />
-        </span>
-      </div>
-    </div>
-    <!-- Message -->
-    <div class="field">
-      <label class="label is-medium">Message</label>
-      <div class="control">
-        <textarea
-          class="textarea"
-          placeholder="Detailed description of your comment, request, etc"
-          rows="8"
-        ></textarea>
-      </div>
-    </div>
-    <div class="control">
-      <button class="button is-primary">
-        Contact Us
-      </button>
-    </div>
-  </form>
   <form name="contact" method="POST" data-netlify="true">
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
