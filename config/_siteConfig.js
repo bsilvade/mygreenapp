f62ab16f5b0829e729643915cc1b3fcf4285a8f4
Bruno Sousa <bruno@Brunos-MBP.lan>
2020-08-05 11:14:20 +0100

    update account id

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -15,7 +15,7 @@ export default {
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id: process.env.UA-174544865-1
+    id: process.env.174544865
   },
   mainMenu: [
     {
