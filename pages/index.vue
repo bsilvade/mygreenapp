2bd0ec25088ea31c8982a365890ba3052fe27d91
Bruno Sousa <bds@outlook.com>
2020-08-28 11:45:57 +0100

    add compressed video

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
       <template v-slot:default>
       <div style="margin:50px,text-align: center">
       <video width="640" height="240" controls autoplay>
-        <source src="/MyGreenApp_SubscribeNewsletter_Version.mp4" type="video/mp4">
+        <source src="/MyGreenApp.mp4" type="video/mp4">
       </video>
         <!-- p class="line-1 anim-typewriter title animated fadeInUp">
         More coming soon... Please stay tuned!
