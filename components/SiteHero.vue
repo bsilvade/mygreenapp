9c6c5fa2f5baefb62b51c743a957ed4b1101c650
Bruno Sousa <bds@outlook.com>
2020-08-28 18:19:40 +0100

    cond

--- components/SiteHero.vue
+++ components/SiteHero.vue
@@ -11,7 +11,7 @@
         <h1 class="title animated fadeInUp">
           {{ title }}
         </h1>
-        <center v-if="showvideo ==='true'">
+        <center v-if="$showvideo ==='true'">
         <video width="640" height="240" autoplay controls poster="/greenapplogo.jpeg">
           <source src="/MyGreenApp.mp4" type="video/mp4">
         </video>
@@ -38,7 +38,9 @@ export default {
     subtitle: { type: String, default: '' },
     image: { type: String, default: '' },
     color: { type: String, default: '#469af0' },
-    theme: { type: String, default: '' }
+    theme: { type: String, default: '' },
+    showvideo: { type: String, default: 'false' }
+
   },
   computed: {
     responsiveImage() {
