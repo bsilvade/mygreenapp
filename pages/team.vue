eea66bb89daf81244912a5ef6e9b0f4161e65e1b
bsilvade <bds@outlook.com>
2021-07-05 21:40:01 +0100

    even larg

--- pages/team.vue
+++ pages/team.vue
@@ -87,7 +87,7 @@
                   :src="require('~/assets/uploads/MGA_20210527.png').src"
                   :srcset="
                     require('~/assets/uploads/MGA_20210527.png').srcSet
-                  " width="900" height="425" 
+                  " width="950" height="425" 
                 />
               
             </div>
